d01cee201477b00e989aacffe217925365c68df6
Mobasshir Ajaz <mobasshirajaz@gmail.com>
2024-01-17 21:36:08 +0530

    Contact form is now operational

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navMenu = document.getElementsByClassName("navmenu");
 navToggle[0].addEventListener("click", function () {
     navMenu[0].classList.toggle("show");
 });
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzxxV-1Mk5of1CwN5snsHeERL9vTIH4WQDqpMWnW9539oa4hggVThC-p9Cto-yhkj8y4g/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
